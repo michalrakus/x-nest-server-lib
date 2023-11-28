b9ddb2f8b53d3ed407789513703593ddb7b88303
michalrakus <michalrakus@gmail.com>
2023-11-28 17:05:00 +0100

    verzia 1.10.0 publishnuta

--- src/services/XEnvVars.ts
+++ src/services/XEnvVars.ts
@@ -1,5 +1,7 @@
 // enum for environment variables in configuration file .env (backend)
 export enum XEnvVar {
+    X_PROTOCOL = 'X_PROTOCOL',
+    X_DOMAIN = 'X_DOMAIN',
     X_PORT = 'X_PORT',
     X_AUTH = 'X_AUTH',
     X_AUTH0_DOMAIN = 'X_AUTH0_DOMAIN',
@@ -8,6 +10,12 @@ export enum XEnvVar {
     X_MS_ENTRA_ID_AUDIENCE = 'X_MS_ENTRA_ID_AUDIENCE',
     X_DATABASE_URL = 'X_DATABASE_URL',
     X_LOG_SQL = 'X_LOG_SQL'
+}
+
+// enum for values of the environment variable X_PROTOCOL
+export enum XProtocol {
+    HTTP = 'HTTP',
+    HTTPS = 'HTTPS'
 }
 
 // enum for values of the environment variable X_AUTH
